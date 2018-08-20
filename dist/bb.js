43f33fa828e5b81362c6a90581f839824ab588e0
Pedro Lauro <pedro.lauro@gmail.com>
2018-08-20 11:16:56 -0300

    fixing header

--- dist/bb.js
+++ dist/bb.js
@@ -42,7 +42,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var apiEndpoint = 'https://mobi.bb.com.br/mov-centralizador/';
 
 var headers = {
-  'User-Agent': 'Android;Google Nexus 5 - 6.0.0 - API 23 - 1080x1920;Android;6.0;vbox86p-userdebug 6.0 MRA58K eng.buildbot.20160110.195928 test-keys;mov-android-app;6.14.0.1;en_US;cpu=0|clock=|ram=2052484 kB|espacoSDInterno=12.46 GB|isSmartphone=true|nfc=false|camera=true|cameraFrontal=true|root=true|reconhecimentoVoz=false|resolucao=1080_1776|densidade=3.0|',
+  'User-Agent': 'Android;Google Nexus 5 - 6.0.0 - API 23 - 1080x1920;Android;6.0;vbox86p-userdebug 6.0 MRA58K eng.buildbot.20160110.195928 test-keys;mov-android-app;6.38.0.6;en_US;cpu=0|clock=|ram=2052484 kB|espacoSDInterno=12.46 GB|isSmartphone=true|nfc=false|camera=true|cameraFrontal=true|root=true|reconhecimentoVoz=false|resolucao=1080_1776|densidade=3.0|',
   'Content-Type': 'application/x-www-form-urlencoded',
   'Cache-Control': 'no-cache',
   'Postman-Token': '7ac0e5e1-8b39-457d-ae08-52c82b41bd5b'
