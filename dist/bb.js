fd536576baa7206b9bd53ad95e9fa60a5d04bccc
Arthur Nobrega <arthurthiago@gmail.com>
2018-10-08 09:57:40 -0300

    Update bb version to 6.41.0.2

--- dist/bb.js
+++ dist/bb.js
@@ -42,7 +42,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var apiEndpoint = 'https://mobi.bb.com.br/mov-centralizador/';
 
 var headers = {
-  'User-Agent': 'Android;Google Nexus 5 - 6.0.0 - API 23 - 1080x1920;Android;6.0;vbox86p-userdebug 6.0 MRA58K eng.buildbot.20160110.195928 test-keys;mov-android-app;6.38.0.6;en_US;cpu=0|clock=|ram=2052484 kB|espacoSDInterno=12.46 GB|isSmartphone=true|nfc=false|camera=true|cameraFrontal=true|root=true|reconhecimentoVoz=false|resolucao=1080_1776|densidade=3.0|',
+  'User-Agent': 'Android;Google Nexus 5 - 6.0.0 - API 23 - 1080x1920;Android;6.0;vbox86p-userdebug 6.0 MRA58K eng.buildbot.20160110.195928 test-keys;mov-android-app;6.41.0.2;en_US;cpu=0|clock=|ram=2052484 kB|espacoSDInterno=12.46 GB|isSmartphone=true|nfc=false|camera=true|cameraFrontal=true|root=true|reconhecimentoVoz=false|resolucao=1080_1776|densidade=3.0|',
   'Content-Type': 'application/x-www-form-urlencoded',
   'Cache-Control': 'no-cache',
   'Postman-Token': '7ac0e5e1-8b39-457d-ae08-52c82b41bd5b'
@@ -257,9 +257,7 @@ var BB = function BB() {
                       year = _session$cabecalho$re2[1];
 
                   return [].concat((0, _toConsumableArray3.default)(acc), (0, _toConsumableArray3.default)(session.celulas.reduce(function (cellAcc, cell) {
-
                     if (cell.TIPO === 'celula' && cell.componentes.length === 3 && cell.componentes[0].componentes[0].texto !== 'Dia') {
-
                       var description = cell.componentes[1].componentes[0].texto;
                       var day = cell.componentes[0].componentes[0].texto;
                       var amount = cell.componentes[2].componentes[0].texto;
--- src/bb.js
+++ src/bb.js
@@ -4,7 +4,7 @@ import querystring from 'querystring'
 const apiEndpoint = 'https://mobi.bb.com.br/mov-centralizador/'
 
 const headers = {
-  'User-Agent': 'Android;Google Nexus 5 - 6.0.0 - API 23 - 1080x1920;Android;6.0;vbox86p-userdebug 6.0 MRA58K eng.buildbot.20160110.195928 test-keys;mov-android-app;6.40.2.0;en_US;cpu=0|clock=|ram=2052484 kB|espacoSDInterno=12.46 GB|isSmartphone=true|nfc=false|camera=true|cameraFrontal=true|root=true|reconhecimentoVoz=false|resolucao=1080_1776|densidade=3.0|',
+  'User-Agent': 'Android;Google Nexus 5 - 6.0.0 - API 23 - 1080x1920;Android;6.0;vbox86p-userdebug 6.0 MRA58K eng.buildbot.20160110.195928 test-keys;mov-android-app;6.41.0.2;en_US;cpu=0|clock=|ram=2052484 kB|espacoSDInterno=12.46 GB|isSmartphone=true|nfc=false|camera=true|cameraFrontal=true|root=true|reconhecimentoVoz=false|resolucao=1080_1776|densidade=3.0|',
   'Content-Type': 'application/x-www-form-urlencoded',
   'Cache-Control': 'no-cache',
   'Postman-Token': '7ac0e5e1-8b39-457d-ae08-52c82b41bd5b',
@@ -138,11 +138,11 @@ export default class BB {
         return [
           ...acc,
           ...session.celulas.reduce((cellAcc, cell) => {
-
-            if (cell.TIPO === 'celula' &&
+            if (
+              cell.TIPO === 'celula' &&
               cell.componentes.length === 3 &&
-              cell.componentes[0].componentes[0].texto !== 'Dia') {
-
+              cell.componentes[0].componentes[0].texto !== 'Dia'
+            ) {
               const description = cell.componentes[1].componentes[0].texto
               const day = cell.componentes[0].componentes[0].texto
               const amount = cell.componentes[2].componentes[0].texto
